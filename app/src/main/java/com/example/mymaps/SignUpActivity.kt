560b2c649e44c16673e05d6bf5da04c2d8993d78
Kooalaa <konsta.alajarvi@gmail.com>
2022-03-23 13:44:24 +0200

    updated toolbar to work on every activity

--- app/src/main/java/com/example/mymaps/SignUpActivity.kt
+++ app/src/main/java/com/example/mymaps/SignUpActivity.kt
@@ -2,17 +2,24 @@ package com.example.mymaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         val tvSignIn = findViewById<TextView>(R.id.tvSignIn)
 
         tvSignIn.setOnClickListener() {
             finish()
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //adds items to the action bar
+        menuInflater.inflate(R.menu.top_bar_menu, menu)
+        return true
     }
 }
