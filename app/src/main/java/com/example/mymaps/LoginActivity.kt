4667dc37ffad42ba54e9de20541153cb742b35dd
hotabir <hotabir90@hotmail.com>
2022-03-29 23:27:52 +0300

    Kirjautumisen/rekisteröitymisen tarkistusta täsmennetty

--- app/src/main/java/com/example/mymaps/LoginActivity.kt
+++ app/src/main/java/com/example/mymaps/LoginActivity.kt
@@ -3,19 +3,37 @@ package com.example.mymaps
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val tvRegister = findViewById<TextView>(R.id.tvRegister)
+        val ivHome = findViewById<ImageView>(R.id.ivHome)
+        val btnSignIn = findViewById<Button>(R.id.btnSignIn)
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        btnSignIn.setOnClickListener() {
+            if (etUsername.text.trim().isNotEmpty() && etPassword.text.isNotEmpty()) {
+                Toast.makeText(this, "Kirjaudutaan sisään...", Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this, "Täytä kaikki kohdat", Toast.LENGTH_LONG).show()
+            }
+        }
 
         tvRegister.setOnClickListener() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+            finish()
         }
+
+        ivHome.setOnClickListener() {
+            finish()
+        }
+
     }
 
 }
--- app/src/main/java/com/example/mymaps/SignUpActivity.kt
+++ app/src/main/java/com/example/mymaps/SignUpActivity.kt
@@ -1,17 +1,49 @@
 package com.example.mymaps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        val etUsernameSU = findViewById<EditText>(R.id.etUsernameSU)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPasswordSU = findViewById<EditText>(R.id.etPasswordSU)
+        val etCPasswordSU = findViewById<EditText>(R.id.etCPasswordSU)
+
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+
         val tvSignIn = findViewById<TextView>(R.id.tvSignIn)
 
+        val ivHomeSU = findViewById<ImageView>(R.id.ivHomeSU)
+
+        btnSignUp.setOnClickListener() {
+            if (etUsernameSU.text.trim().isNotEmpty() && etPasswordSU.text.isNotEmpty() &&
+                etEmail.text.trim().isNotEmpty() && etCPasswordSU.text.isNotEmpty()) {
+                if (etPasswordSU.text.toString() == etCPasswordSU.text.toString()) {
+                    Toast.makeText(this, "Luodaan profiili...", Toast.LENGTH_LONG).show()
+                }
+                else {
+                    Toast.makeText(this, "Salasanat eivät täsmää", Toast.LENGTH_LONG).show()
+                }
+            }
+            else {
+                Toast.makeText(this, "Täytä kaikki kohdat", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
         tvSignIn.setOnClickListener() {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        ivHomeSU.setOnClickListener() {
             finish()
         }
     }
