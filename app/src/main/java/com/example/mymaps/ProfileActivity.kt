5a1a1132d066998b84413cf0d7a9ee8f0f67df2f
Kooalaa <konsta.alajarvi@gmail.com>
2022-04-04 09:59:02 +0300

    Added way to update profile page dynamically

--- app/src/main/java/com/example/mymaps/ProfileActivity.kt
+++ app/src/main/java/com/example/mymaps/ProfileActivity.kt
@@ -1,9 +1,20 @@
 package com.example.mymaps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.ColorSpace
+import android.icu.number.IntegerWidth
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.view.Gravity
 import android.view.Menu
-import android.view.MenuItem
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import java.lang.reflect.Array
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +22,9 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        var list = arrayOf("keijo", "ismo", "lauri")
+        updateGrid(list)
+        updateInfo(list.size.toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,5 +36,25 @@ class ProfileActivity : AppCompatActivity() {
         menu?.findItem(R.id.profile)?.setVisible(false)
         menu?.findItem(R.id.icLogin)?.setVisible(false)
         return super.onPrepareOptionsMenu(menu)
+    }
+
+
+    // Päivitetään käyttäjän "kupongit"
+    private fun updateGrid(list: kotlin.Array<String>) {
+        for (i in list.indices) {
+            val button = Button(this)
+            button.setBackgroundColor(getColor(R.color.primary))
+            button.setText(list[i])
+            button.height = 200
+            button.width = 500
+            button.gravity = Gravity.CENTER
+            findViewById<GridLayout>(R.id.grid).addView(button)
+        }
+    }
+
+    // Päivitetään käyttäjän info
+    private fun updateInfo(quantity: String) {
+        val textView = findViewById<TextView>(R.id.coupon_quantity)
+        textView.text = quantity
     }
 }
